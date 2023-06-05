73ce5c682eb9ddb190cac6b4162edb322951fc13
hedi72 <heditaieb72@gmail.com>
2023-06-05 08:11:04 -0700

    fes

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print('Programming is like building a multilingual puzzle')
+
+# Print the message with double quotation marks
+print('"Programming is like building a multilingual puzzle"')
