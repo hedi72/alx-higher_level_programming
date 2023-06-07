380bcbf1f036dde5714c30bb03cf969c9c669deb
hedi72 <heditaieb72@gmail.com>
2023-06-07 02:56:53 -0700

    fixed programm that print all possible different combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 8):
     for j in range(0, 10):
-        print("{:d}{:d}, ".format(i, j), end="")
+        if i < j:
+            print("{:d}{:d}, ".format(i, j), end="")
 print(89)
