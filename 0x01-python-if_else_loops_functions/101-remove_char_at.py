3ae6ff824a585c820fcca410685deb03db2d101d
hedi72 <heditaieb72@gmail.com>
2023-06-08 05:55:49 -0700

    function that create a copy

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     car = " "
-    for i in range(len(str)):
+    for i in range(0, len(str)):
         if i != n:
             car += str[i]
     return car
