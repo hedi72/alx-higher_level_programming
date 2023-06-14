665c6228ee354dbef551516e2c1875104580629f
hedi72 <heditaieb72@gmail.com>
2023-06-14 09:18:52 -0700

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_matrix = my_list[:]
     for element in new_matrix:
-        if element == 2:
+        if element == search:
             new_matrix[element] = replace
     return  new_matrix
