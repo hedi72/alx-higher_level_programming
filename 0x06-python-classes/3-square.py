8e9e8250d7c73f1d28e4838af2cc8094b17ffe3c
hedi72 <heditaieb72@gmail.com>
2023-07-05 05:40:17 -0700

    area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,9 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
 
         """Return the current area of the square."""
+
         return (self.__size * self.__size)
