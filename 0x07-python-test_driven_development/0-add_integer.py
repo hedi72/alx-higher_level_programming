300b65d06cbd5ab78c0cb18311d94d6be7e0c7ea
hedi72 <heditaieb72@gmail.com>
2023-07-20 02:51:36 -0700

    fixed integers addition function and doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 This module contains one function:
-
 add_integer(a, b)
 """
+
+
 def add_integer(a, b=98):
     """Add two integers
     Args:
@@ -12,9 +13,10 @@ def add_integer(a, b=98):
     Returns:
         int: sum of two passed integers
     """
-    if not isinstance(a,(int, float)):
+
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int,float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         return int(a + b)
