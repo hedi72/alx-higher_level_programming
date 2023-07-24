3d1f8faf3021333bc8c1cd5e1552e1f98bdddf89
hedi72 <heditaieb72@gmail.com>
2023-07-24 06:16:09 -0700

    fixed function text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+# !/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    separators = ['.', '?', ':']
+    current_sentence = ''
+    
+    for char in text:
+        current_sentence += char
+        
+        if char in separators:
+            print(current_sentence.strip())
+            current_sentence = ''
+
+    if current_sentence:
+        print(current_sentence.strip())
