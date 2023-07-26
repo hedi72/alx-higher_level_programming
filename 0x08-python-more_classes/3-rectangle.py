15999e717d11c3b55f209b8cef3454e8b453aa50
hedi72 <heditaieb72@gmail.com>
2023-07-26 04:19:54 -0700

    add print()

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,60 +1,45 @@
-#!/usr/bin/python3
-""" module contains one class """
-
-
 class Rectangle:
-    """ class rectangle """
-    
     def __init__(self, width=0, height=0):
-        """ instantiation """
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """ getter for width """
         return self.__width
-
-    @property
-    def height(self):
-        """ getter for height """
-        return self.__height
 
     @width.setter
     def width(self, value):
-        """ setter for width """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """ setter for height """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
+
     def area(self):
         return self.width * self.height
+
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
-            return 0
-        else:
-            return (self.width + self.height) * 2
-            
+        return 2 * (self.width + self.height)
+
     def __str__(self):
-        
         if self.width == 0 or self.height == 0:
             return ""
         rect_str = ""
-        
-        for i in range(self.height):
-            for j in range(self.width):
-                rect_str += "#" 
-            rect_str += "\n"
-    
-        return rect_str
+        for _ in range(self.height):
+            rect_str += "#" * self.width + "\n"
+        return rect_str[:-1]  # Remove the last newline
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
