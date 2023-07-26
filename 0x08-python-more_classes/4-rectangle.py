edab53060d160a23ab748c8c54b0e47e562b9981
hedi72 <heditaieb72@gmail.com>
2023-07-26 05:55:16 -0700

    fixed repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,46 +1,64 @@
 #!/usr/bin/python3
+"""
+module contains one class
+"""
+
+
 class Rectangle:
+    """ class rectangle """
     def __init__(self, width=0, height=0):
+        """ instantiation """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ getter for width """
         return self.__width
+
+    @property
+    def height(self):
+        """ getter for height """
+        return self.__height
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        """ setter for width """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
-    def height(self):
-        return self.__height
-
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        """ setter for height """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        return self.width * self.height
+        """ returns the area of the rectangle """
+        return self.__height * self.__width
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        """ returns the perimeter of the rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__height + self.__width) * 2
 
     def __str__(self):
-        if self.width == 0 or self.height == 0:
-            return ""
-        rect_str = ""
-        for _ in range(self.height):
-            rect_str += "#" * self.width + "\n"
-        return rect_str[:-1]  
+        """ returns a nicely printable representation """
+        ch = ""
+        if self.__width == 0 or self.__height == 0:
+            return ch
+        for i in range(self.__height):
+            for j in range(self.__width):
+                ch += "#"
+            ch += '\n'
+        return ch[:-1]
 
     def __repr__(self):
         """ returns a string similar to code of generating new object """
