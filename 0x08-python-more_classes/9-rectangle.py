4ed8037749bcf1cdb3a512b647973db1d8bf1151
hedi72 <heditaieb72@gmail.com>
2023-07-26 11:25:17 -0700

    fixed class method that return a new rectangle instance

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -83,7 +83,7 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-    
-    @classmethod        
+
+    @classmethod
     def square(cls, size=0):
         return cls(size, size)
