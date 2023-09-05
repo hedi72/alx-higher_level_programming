fb5b31f5ddd1bfb8d7d17a37b782a08e48ff4881
hedi72 <heditaieb72@gmail.com>
2023-09-05 13:34:19 -0700

    task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains definition for class LockedClass.
+"""
+
+
+class LockedClass:
+    """Definition of class LockedClass that only defines one attribute"""
+
+    __slots__ = ['first_name']
