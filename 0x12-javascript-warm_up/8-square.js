8d3c6af7e052fe054f841e57370e161500a6dec7
hedi72 <heditaieb72@gmail.com>
2023-11-13 14:24:01 -0800

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,9 @@
 
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size)) {
-  console.log("Missing size");
+if (isNaN(size) || process.argv[2] === undefined) {
+    console.log('Missing size');
+    
 } else {
   if (size > 0) {
     for (let i = 0; i < size; i++) {
