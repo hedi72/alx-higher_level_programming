4a927f8a4e60579e31d45a5b3641650852f2d362
hedi72 <heditaieb72@gmail.com>
2023-11-13 08:36:39 -0800

    task4

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,5 @@
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  for (let index = 2; index < process.argv.length; index++) {
-    console.log(process.argv[index]);
-  }
+  console.log(process.argv[2]);
 }
