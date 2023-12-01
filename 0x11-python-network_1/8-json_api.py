e031dc1537950e9e7ed485081b2d14a355a6e62a
hedi72 <heditaieb72@gmail.com>
2023-12-01 12:25:21 -0800

    task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" module doc """
+"""
+a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ''
-    url = 'http://0.0.0.0:5000/search_user'
-    params = {'q': q}
-    res = requests.post(url, data=params)
+
+if __name__ == '__main__':
     try:
-        resjson = res.json()
-        if resjson:
-            print(f'[{resjson['id']}] {resjson['name']}')
-        else:
-            print('No result')
-    except Exception:
+        qv = sys.argv[1]
+    except:
+        qv = ''
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': qv})
+    try:
+        print('[{}] {}'.format(r.json()['id'], r.json()['name']))
+    except ValueError:
         print('Not a valid JSON')
+    except:
+        print('No result')
